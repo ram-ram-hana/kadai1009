8e7f405eae4002d28f4c10ef2be13ea51340fdb2
ram-ram-hana <ram.ram.hana@gmail.com>
2019-11-06 05:27:23 +0000

    課題を追加

---
+++ kadai1030/indexkadai5.js
@@ -0,0 +1,13 @@
+setTime(
+   () => {
+       console.log('Hello!')
+   },
+   3000
+)
+
+setTimeout(
+    () => {
+        console.log('Bye!')
+    },
+    2000
+)
--- kadai1030/test.js
+++
@@ -1,5 +0,0 @@
- function add (a,b) {
-      console.log(a + b)
- }
- add(1,2)
- add(5,10)
